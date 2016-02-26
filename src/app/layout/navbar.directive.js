0587249d7eea12b1b7a878665418150be70a1549
alanbares <neloice@gmail.com>
2016-02-26 12:58:31 -0800

    Add directive controller to abNavbar and remove vm.logout and vm.isLoggedIn in AuthController

--- src/app/layout/navbar.directive.js
+++ src/app/layout/navbar.directive.js
@@ -8,7 +8,23 @@
     function abNavbar() {
         return {
             templateUrl: 'app/layout/navbar.html',
-            restrict: 'E'
+            restrict: 'E',
+            controller: NavbarController,
+            controllerAs: 'vm'
         };
+    }
+
+    NavbarController.$inject = ['$location', 'authService'];
+
+    function NavbarController($location, authService) {
+        var vm = this;
+
+        vm.isLoggedIn = authService.isLoggedIn;
+        vm.logout = logout;
+
+        function logout() {
+            authService.logout();
+            $location.path('/');
+        }
     }
 })();
